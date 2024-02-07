92595f79691b862a74e1d5f314971d0f1747d732
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-02-07 19:57:57 -0300

    add sensor

--- led.js
+++ led.js
@@ -1,12 +1,17 @@
 var onoff = require('onoff');
 var Gpio = onoff.Gpio,
 led = new Gpio(4, 'out'),
+sensor = new Gpio(17, 'in'),
 
 interval;
 
 interval = setInterval(function () {
 
 var value = (led.readSync() + 1) % 2;
+var sensorValue = sensor.readSync();
+if (sensorValue != 0) {
+console.log("Sensor value: " + sensorValue);
+}
 led.write(value, function() {
 
 console.log("Changed LED state to: " + value);
