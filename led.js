bdeb8b048c4523dfc8a716ee28b0aae07849b8ff
Elisson Rodrigo <ersa@cin.ufpe.br>
2024-02-07 20:18:13 -0300

    add buzzer

--- led.js
+++ led.js
@@ -1,32 +1,10 @@
-var onoff = require('onoff');
-var Gpio = onoff.Gpio,
-led = new Gpio(4, 'out'),
-sensor = new Gpio(17, 'in'),
+from gpiozero import Buzzer
+from time import sleep
 
-interval;
+buzzer = Buzzer(17)
 
-interval = setInterval(function () {
-
-var value = (led.readSync() + 1) % 2;
-var sensorValue = sensor.readSync();
-if (sensorValue != 0) {
-console.log("Sensor value: " + sensorValue);
-}
-led.write(value, function() {
-
-console.log("Changed LED state to: " + value);
-
-});
-
-}, 2000);
-process.on('SIGINT', function () {
-
-clearInterval(interval);
-led.writeSync(0);
-
-led.unexport();
-console.log('Bye, bye!');
-
-process.exit();
-
-});
+while True:
+    buzzer.on()
+    sleep(1)
+    buzzer.off()
+    sleep(1)
